1329af76619a25f84fd6121b3ba2f75436447015
Sandluminat <sdfox@gmx.ch>
2022-09-14 14:56:38 -0700

    Updated word frequency aspect and README

--- backend/comspacity_backend.py
+++ backend/comspacity_backend.py
@@ -1,4 +1,5 @@
 import spacy
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -24,11 +25,11 @@ async def create_item(txt: Text, language: str):
 
     sentences = 0
     words = 0
-    word_list_frequency = []
-    word_list_test =[]
     words_length = 0
     verbes = 0
-    adjectives = 0
+    with open('sample.json', 'r') as openfile:
+        json_object = json.load(openfile)
+    hard_words = 0
 
     for line in doc.sents:
         for token in line:
@@ -37,36 +38,36 @@ async def create_item(txt: Text, language: str):
                 words += 1
                 words_length += len(token)
 
-                if spacy.explain(token.pos_) == "adjective":
-                    adjectives += 1
-                elif token.pos_ == "VERB":
+                if token.pos_ == "VERB":
                     verbes += 1
                 
-                if token.lemma_ in word_list_test:
-                    word_index = word_list_test.index(token.lemma_)
-                    word_list_frequency[word_index][1] += 1
-
+                if token.text in json_object:
+                    pass
                 else:
-                    word_list_frequency.append([token.lemma_, 1])
-                    word_list_test.append(token.lemma_)
+                    hard_words += 1
 
 
             if spacy.explain(token.tag_) == "punctuation mark, sentence closer" or spacy.explain(token.tag_) == "sentence-final punctuation mark":
                 sentences += 1
     
     average_word_length = words_length/words
-    average_word_frequency = words/len(word_list_test)
     words_per_sentence = words/sentences
+    average_hard_words = hard_words/words
     verbes_per_sentence = verbes/sentences
-    word_list_frequency.sort(key=lambda x: (-x[1]))
 
     # language distinction 2
     if language == "english":
-        complexity_of_text = words_per_sentence/17.5*38 + verbes_per_sentence/1.5*12 + average_word_length/4.7*60 - average_word_frequency/3.5*10
+        complexity_of_text = words_per_sentence/17.5*35 + verbes_per_sentence/1.5*10 + average_word_length/4.7*55 + average_hard_words*10 
     elif language == "german":
-        complexity_of_text = words_per_sentence/17.5*38 + verbes_per_sentence/1.5*12 + average_word_length/5*60 - average_word_frequency/3.5*10
+        complexity_of_text = words_per_sentence/17.5*38 + verbes_per_sentence/1.5*12 + average_word_length/5*60
+    
+    output = {
+        "average hard words": average_hard_words,
+        "content": txt.texts,
+        "complexityscore": complexity_of_text,
+        }
 
-    return complexity_of_text, sentences, words, words_per_sentence, average_word_length, average_word_frequency, verbes_per_sentence, verbes, adjectives, word_list_frequency
+    return output
 
 @app.post("/complexity/sentences/{language}")
 async def create_item(txt: Text, language: str):
@@ -79,14 +80,17 @@ async def create_item(txt: Text, language: str):
         nlp = spacy.load("de_core_news_sm")
     doc = nlp(text)
 
-    words = 0
-    words_length = 0
-    verbes = 0
-    adjectives = 0
+    with open('sample.json', 'r') as openfile:
+        json_object = json.load(openfile)
     sentences = []
 
     for line in doc.sents:
         sentence = line.text
+        words = 1
+        words_length = 0
+        verbes = 0
+        hard_words = 0
+
         for token in line:
             # passives
             if spacy.explain(token.pos_) != "punctuation" and token.lemma_ != " ":
@@ -95,23 +99,32 @@ async def create_item(txt: Text, language: str):
                 else:
                     words += 1
                     words_length += len(token)
-                    if spacy.explain(token.pos_) == "adjective":
-                        adjectives += 1
                     if token.pos_ == "VERB":
                         verbes += 1
+
+                    if token.text in json_object:
+                        pass
+                    else:
+                        hard_words += 1
+                    
             if spacy.explain(token.tag_) == "punctuation mark, sentence closer" or spacy.explain(token.tag_) == "sentence-final punctuation mark":
                 average_word_length = words_length/words
+                average_hard_words = hard_words/words
+
 
                 # Language distinction 2.
                 if language == "english":
-                    complexity_of_sentence = words/17.5*38 + verbes/1.5*12 + average_word_length/4.7*60
+                    complexity_of_sentence = words/17.5*38 + verbes/1.5*12 + average_word_length/4.7*60 + average_hard_words*100
                 elif language == "german":
                     complexity_of_sentence = words/17.5*38 + verbes/1.5*12 + average_word_length/5*60
                 
-                sentences.append([sentence, complexity_of_sentence, words, average_word_length, verbes, adjectives])
-                words = 0
+                sentences.append([sentence, complexity_of_sentence])
+                words = 1
                 words_length = 0
                 verbes = 0
-                adjectives = 0
-    sentences.sort(key = lambda x: -x[1])   
-    return sentences
+                hard_words = 0
+
+    sentences.sort(key = lambda x: -x[1])
+    complexity_json = {sentences[i][0]: sentences[i][1] for i in range(len(sentences))}
+
+    return complexity_json
